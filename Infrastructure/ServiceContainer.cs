14846fb9fd448f290d6954f6aed690668f123914
zied kefi <zied.kefi@gmail.com>
2025-04-17 09:28:14 +0100

    ServiceContainer Adjustment

--- Infrastructure/ServiceContainer.cs
+++ Infrastructure/ServiceContainer.cs
@@ -5,6 +5,7 @@ using Infrastructure.PersistenceDb;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+namespace Infrastructure;
 
 public static class ServiceContainer
 {
